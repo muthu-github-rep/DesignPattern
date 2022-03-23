6b264008d9d5574bce23640c778c8ebac60e37ac
muthu-github-rep <102046225+muthu-github-rep@users.noreply.github.com>
2022-03-23 22:56:25 +0530

    Update

--- src/StrategyPattern/CalculateTradeAmount.java
+++ src/StrategyPattern/CalculateTradeAmount.java
@@ -3,12 +3,9 @@ package StrategyPattern;
 public class CalculateTradeAmount {
     private TradeAmountStrategy strategy;
 
-    // On object creation specify which strategy (functional interface) to use
     public CalculateTradeAmount(TradeAmountStrategy strategy) {
            this.strategy = strategy;
     }
-
-    // Execute the functional interfaces lambda expression implementation
     public void exec() {
          strategy.execute();
     }
